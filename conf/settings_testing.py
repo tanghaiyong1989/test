d98778a980aec912a7277a88c825cf3df37860c5
HaiYong Tang <807808055@qq.com>
2019-08-14 12:14:09 +0800

    update

--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -13,8 +13,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
         'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                            # 你的数据库user
-        'PASSWORD': 'yvhitxcel',                        # 你的数据库password
-        'HOST': '172.16.126.50',                   		   # 数据库HOST
+        'PASSWORD': '',                        # 你的数据库password
+        'HOST': '127.0.0.1',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
     },
 }
--- home_application/urls.py
+++ home_application/urls.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import url,include
+# from django.conf.urls import url,include
 from django.conf.urls import patterns
 
 urlpatterns = patterns(
     'home_application.views',
     (r'^$', 'home'),
-    (r'^dev-guide/$', 'dev_guide'),
+    # (r'^dev-guide/$', 'dev_guide'),
     (r'^contactus/$', 'contactus'),
-    url(r'^test1/', include('test1.urls')),
+    (r'^helloworld/$', 'helloworld'),
 )
